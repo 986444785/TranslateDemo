e153036241cfd7b7eb8d35e9ac018ac556622d82
Dragon <zangqilong@gmail.com>
2016-10-10 15:47:09 +0800

    Initial Commit

---
+++ TranslateDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TranslateDemo
+//
+//  Created by zang qilong on 10/10/16.
+//  Copyright © 2016 zang qilong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
